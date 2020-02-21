f5780fad03bc55c28ddf14e69daea58e7346c53d
monosoul <andrei.nevedomskii@n26.com>
2020-02-21 16:33:47 +0100

    Configure test logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,22 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+
 group = "com.github.monosoul"
 version = "0.0.1"
 
 subprojects {
     repositories {
         mavenCentral()
+    }
+
+    tasks {
+        withType<Test> {
+            testLogging {
+                events(PASSED, SKIPPED, FAILED)
+                exceptionFormat = FULL
+            }
+        }
     }
 }
