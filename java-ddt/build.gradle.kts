e38fa19b3ab6dcaba36c2a58e03aec596eb8e251
monosoul <andrei.nevedomskii@n26.com>
2020-02-21 16:20:45 +0100

    Set compile target to JVM 11

--- java-ddt/build.gradle.kts
+++ java-ddt/build.gradle.kts
@@ -2,3 +2,8 @@ plugins {
     `java-library`
     groovy
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
--- kotlin-ddt/build.gradle.kts
+++ kotlin-ddt/build.gradle.kts
@@ -1,3 +1,11 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.3.61"
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
 }
